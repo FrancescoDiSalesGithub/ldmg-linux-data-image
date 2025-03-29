a5224c850ea89e0f3efe496cd8f67ecb34472b4c
Francesco Di Sales <f.disales@hotmail.com>
2025-03-29 18:49:50 +0100

    Create boot-ldmg.sh

---
+++ boot-ldmg.sh
@@ -0,0 +1,17 @@
+# !/bin/bash
+
+user=$(whoami)
+
+if [ "$user" == "root" ]
+then
+
+  cp ldmg.xml /usr/share/mime/packages/
+  update-mime-database /usr/share/mime
+  
+  cp ldmg-mounter.desktop /usr/share/applications/
+  cp mount-ldmg.sh /usr/local/bin/
+
+  chmod 777 /usr/local/bin/mount-ldmg.sh 
+
+else
+echo "run this script as root user"
