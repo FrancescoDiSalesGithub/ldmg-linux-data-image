63d28272590b796acfe1d0ea815244bb703d599e
Francesco Di Sales <f.disales@hotmail.com>
2025-03-29 19:34:28 +0100

    Update mount-ldmg.sh

--- mount-ldmg.sh
+++ mount-ldmg.sh
@@ -10,10 +10,5 @@ LOOP_DEV=$(losetup -f)
 pkexec losetup "$LOOP_DEV" "$LDMG_FILE";mkdir -p "$MOUNT_POINT";mount "$LOOP_DEV"  "$MOUNT_POINT";udisksctl mount -b "$LOOP_DEV"
 
 
-if [ $? -eq 0 ]; then
-    notify-send "LDMG Mounted" "$LDMG_FILE mounted at $MOUNT_POINT"
-else
-    notify-send "LDMG Mount Failed" "Could not mount $LDMG_FILE"
-fi
 
                                 
